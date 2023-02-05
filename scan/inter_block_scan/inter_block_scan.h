4f7e28b7ba99784627a8f51f16a887e00f6d4b1a
z12fuala <117768899+z12fuala@users.noreply.github.com>
2023-02-05 22:28:25 +0100

    Update inter_block_scan.h

--- scan/inter_block_scan/inter_block_scan.h
+++ scan/inter_block_scan/inter_block_scan.h
@@ -6,40 +6,40 @@
 
 // Exclusive inter-block scan
 __device__ static void exc_inter_block_scan(// output
-											volatile ull &s_prefix_sum,
-											// input
-											uint value,
-											uint value_idx,
-											int warp_lane,
-											int warp_idx,
-											volatile ull *d_scan,
-											uint num_values) {
+                                            volatile ull &s_prefix_sum,
+                                            // input
+                                            uint value,
+                                            uint value_idx,
+                                            int warp_lane,
+                                            int warp_idx,
+                                            volatile ull *d_scan,
+                                            uint num_values) {
 	int segment_idx, segment_lane, segment_last_lane;
 	calc_segment_param(// output
-					   segment_idx, segment_lane, segment_last_lane,
-					   // input
-					   value_idx, num_values);
+                           segment_idx, segment_lane, segment_last_lane,
+                           // input
+                           value_idx, num_values);
 
 	ull segment_sum;
 	sum_value_to_segment(// output
-						 d_scan, segment_sum,
-						 // input
-						 value, segment_idx, segment_lane,
-						 warp_lane, segment_last_lane,
-						 num_values);
+                             d_scan, segment_sum,
+                             // input
+                             value, segment_idx, segment_lane,
+                             warp_lane, segment_last_lane,
+                             num_values);
 
 	ull sum_of_prev_values_within_segment;
 	get_sum_of_prev_values_within_segment(// output
-										  sum_of_prev_values_within_segment,
-										  // input
-										  segment_idx, segment_lane,
-										  d_scan);
+                                              sum_of_prev_values_within_segment,
+                                              // input
+                                              segment_idx, segment_lane,
+                                              d_scan);
 
 	ull sum_of_prev_segments;
 	get_sum_of_prev_segments(// output
-							 sum_of_prev_segments,
-							 // input
-							 d_scan, segment_idx);
+                                 sum_of_prev_segments,
+                                 // input
+                                 d_scan, segment_idx);
 
 	s_prefix_sum = sum_of_prev_values_within_segment + sum_of_prev_segments;
 
@@ -47,10 +47,10 @@ __device__ static void exc_inter_block_scan(// output
 		ull sum_of_segments_0_to_current = sum_of_prev_segments + segment_sum;
 
 		write_sum_of_segments_0_to_current(// output
-										   d_scan,
-										   // input    
-										   segment_idx,
-										   sum_of_segments_0_to_current);
+                                                   d_scan,
+                                                   // input    
+                                                   segment_idx,
+                                                   sum_of_segments_0_to_current);
 	}
 }
 
