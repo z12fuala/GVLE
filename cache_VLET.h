9a1c4b57ff326fc8857e52842705122f1cd4e53e
z12fuala <117768899+z12fuala@users.noreply.github.com>
2023-02-05 22:15:16 +0100

    Update cache_VLET.h

--- cache_VLET.h
+++ cache_VLET.h
@@ -2,11 +2,11 @@
 
 template <uint TBLOCK_SIZE>
 __device__ static void cache_VLET(// output
-								  ushort s_VLET_val[256],
-								  uchar s_VLET_len[256],
-								  // input  
-								  ushort d_VLET_val[256],
-								  uchar d_VLET_len[256]) {
+                                  ushort s_VLET_val[256],
+                                  uchar s_VLET_len[256],
+                                  // input  
+                                  ushort d_VLET_val[256],
+                                  uchar d_VLET_len[256]) {
 	// Copy d_VLET_val/d_VLET_len to s_VLET_val/s_VLET_len
 	// coalescently
 	for (int i = threadIdx.x; i < 256; i += TBLOCK_SIZE) {
