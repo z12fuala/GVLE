38cfb330edb91e566697c048e2f9ef013c7041e7
z12fuala <117768899+z12fuala@users.noreply.github.com>
2023-02-05 22:31:09 +0100

    Update write_sum_of_segments_0_to_current.h

--- scan/inter_block_scan/write_sum_of_segments_0_to_current.h
+++ scan/inter_block_scan/write_sum_of_segments_0_to_current.h
@@ -1,9 +1,9 @@
 #include "sum_field_macros.h"
 
 __device__ static void write_sum_of_segments_0_to_current(// output
-														  volatile ull *d_scan,
-														  // input            
-														  int segment_idx,
-														  ull sum_of_segments_0_to_current) {
+                                                          volatile ull *d_scan,
+                                                          // input            
+                                                          int segment_idx,
+                                                          ull sum_of_segments_0_to_current) {
 	d_scan[segment_idx * WARP_SIZE + WARP_SIZE - 1] = SET_FLAG_P(sum_of_segments_0_to_current);
 }
