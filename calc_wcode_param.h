d7acc66b1408963e970e341885aec8d38331e828
z12fuala <117768899+z12fuala@users.noreply.github.com>
2023-02-05 22:20:07 +0100

    Update calc_wcode_param.h

--- calc_wcode_param.h
+++ calc_wcode_param.h
@@ -5,21 +5,21 @@
 
 template <uint TBLOCK_SIZE>
 __device__ static void calc_wcode_param(// output
-										uint &pos_of_thcode_in_wcode,
-										uint &wcode_len,
-										uint &wcode_pos,
-										// input
-										uint thcode_len,
-										ull *d_scan,
-										uint blockinput_idx,
-										uint num_blockinputs,
-										int warp_idx, int warp_lane) {
+                                        uint &pos_of_thcode_in_wcode,
+                                        uint &wcode_len,
+                                        uint &wcode_pos,
+                                        // input
+                                        uint thcode_len,
+                                        ull *d_scan,
+                                        uint blockinput_idx,
+                                        uint num_blockinputs,
+                                        int warp_idx, int warp_lane) {
 	// Calculate the bit-positions of the thread-codes in their warp-code
 	// and the bit-length of the warp-code	
 	exc_warp_scan<WARP_SIZE>(// output
-							 pos_of_thcode_in_wcode, wcode_len,
-							 // input
-							 thcode_len, warp_lane);
+                                 pos_of_thcode_in_wcode, wcode_len,
+                                 // input
+                                 thcode_len, warp_lane);
 
 	// Write the bit-length of the warp-code to shared memory
 	__shared__ uint s_scan[NUM_WARPS(TBLOCK_SIZE)];
@@ -36,9 +36,9 @@ __device__ static void calc_wcode_param(// output
 		uint wcode_len_inc_scan;
 		uint blockcode_len;
 		inc_warp_scan<NUM_WARPS(TBLOCK_SIZE)>(// output
-											  wcode_len_inc_scan, blockcode_len,
-											  // input
-											  wcode_len, warp_lane);
+                                                      wcode_len_inc_scan, blockcode_len,
+                                                      // input
+                                                      wcode_len, warp_lane);
 
 		// Write the bit-positions calculated in previous step to shared memory
 		if (warp_lane < NUM_WARPS(TBLOCK_SIZE)) {
@@ -47,11 +47,11 @@ __device__ static void calc_wcode_param(// output
 
 		// Calculate the bit-position of the blockcode in the output vector
 		exc_inter_block_scan(// output
-							 s_blockcode_pos,
-							 // input
-							 blockcode_len, blockinput_idx,
-							 threadIdx.x, warp_idx,
-							 d_scan, num_blockinputs);
+                                     s_blockcode_pos,
+                                     // input
+                                     blockcode_len, blockinput_idx,
+                                     threadIdx.x, warp_idx,
+                                     d_scan, num_blockinputs);
 	}
 
 	__syncthreads();
